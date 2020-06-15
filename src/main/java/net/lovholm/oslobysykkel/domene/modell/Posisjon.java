74e1069e45a9fe8d07a13760a9c76f8548e263ac
olovholm <ola.lovholm@nav.no>
2020-06-15 17:31:55 +0200

    Skiller ut Posisjon i eget verdiobjekt

---
+++ src/main/java/net/lovholm/oslobysykkel/domene/modell/Posisjon.java
@@ -0,0 +1,26 @@
+package net.lovholm.oslobysykkel.domene.modell;
+
+import net.lovholm.oslobysykkel.domene.Haversine;
+
+public class Posisjon {
+    private final Double lat;
+    private final Double lon;
+
+    public Posisjon(Double lat, Double lon){
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public Double distanseFra(Posisjon annenPosisjon){
+        return Haversine.haversine(this.lat, this.lon, annenPosisjon.lat, annenPosisjon.lon);
+    }
+
+}
